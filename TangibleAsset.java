df0e07178010abce2d77ab4d1f25762c0b53567a
zenyan357 <147118963+zenyan357@users.noreply.github.com>
2023-10-10 10:34:04 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,15 @@
+public abstract classTangibleAsset {
+  private String name;
+  private int price;
+  private String color;
+  
+  public TangibleAsset(String name, int price, String color) {
+    this.name = name;
+    this.price = price;
+    this.color = color;
+  }
+  public String getName(){ return this.name; }
+  public int price(){ return this.price; }
+  public String getColor(){ return this.color; }
+
+} 
